b1c317b171e2cf977bab12ca1f00bea0034011f2
Divyaswor Makai <makaidivya@gmail.com>
2020-03-18 21:12:43 +0545

    Two way translation

--- MorseCodeTranslator.py
+++ MorseCodeTranslator.py
@@ -2,20 +2,60 @@ from Tree import TreeDeclaration
 
 mainTree = TreeDeclaration()
 
-string = '..-. ..- -.-. -.-  -.-- --- ..-'
-parts = string.split('  ')
-finalStr = ''
-for part in parts:
-	words = part.split(' ')
-	for word in words:
-		currNode = mainTree.root
-		for elem in word:
-			if(elem == '.'):
-				currNode = currNode.left
-			elif(elem == '-'):
-				currNode = currNode.right
-		finalStr+=currNode.value
-	finalStr+=" "
+def ChangeToEnglish(string):
+	parts = string.split('  ')
+	finalStr = ''
+	for part in parts:
+		words = part.split(' ')
+		for word in words:
+			currNode = mainTree.root
+			for elem in word:
+				if elem == '.' and currNode.left.value!= '_':
+					currNode = currNode.left
+				elif elem == '-' and currNode.right.value!= '_':
+					currNode = currNode.right
+			finalStr += currNode.value
+		finalStr+=" "
+	print(finalStr)
 
-print(finalStr)
+def ChangeToMorseCode(string):
+	parts = string.split(' ')
+	finalCode = ''
+	for part in parts:
+		currMorse = ''
+		for letter in part:
+			rootNode = mainTree.root
+			currMorse = SearchInTree(letter)
 
+def SearchInTree(letter):
+	currNode = mainTree.root
+	totalList = list()
+	currMorse = ''
+	totalList.append([currNode,currMorse])
+	complete = False
+	counter = 0
+
+	#A Simple BFS of the tree to the find the coorect morse code for the alphabet
+	while not complete:
+		currNode = totalList[counter]
+		currMorse = currNode[1]
+		if currNode[0].value == letter:
+			complete = True
+			print(currMorse)
+			break
+
+		if currNode[0].depth<=4:
+			if currNode[0].left!=None:
+				temp = currMorse
+				temp += '.'
+				totalList.append([currNode[0].left,temp])
+
+			if currNode[0].right!= None:
+				temp = currMorse
+				temp +='-'
+				totalList.append([currNode[0].right,temp])
+		counter+=1
+
+if __name__ == '__main__':
+	ChangeToEnglish('.... ..- ---')
+	ChangeToMorseCode('huo')
--- Tree.py
+++ Tree.py
@@ -1,4 +1,5 @@
 from string import ascii_lowercase
+import copy
 
 class tree:
 	def __init__(self):
@@ -15,6 +16,7 @@ def TreeDeclaration():
 	# Tree declaration
 	mainTree = tree()
 	Root = mainTree.Node('_')
+	Empty = mainTree.Node('_')
 	mainTree.root = Root
 
 	# Declaration of the Nodes
@@ -32,49 +34,127 @@ def TreeDeclaration():
 
 	Root.left = di['e']
 	Root.right = di['t']
+	Root.depth = 0
 
 	di['e'].left = di['i']
 	di['e'].right = di['a']
+	di['e'].depth = 1
 
 	di['t'].left = di['n']
 	di['t'].right = di['m']
+	di['t'].depth = 1
 
 	di['i'].left = di['s']
 	di['i'].right = di['u']
+	di['i'].depth = 2
 
 	di['a'].left = di['r']
 	di['a'].right = di['w']
+	di['a'].depth = 2
 
 	di['n'].left = di['d']
 	di['n'].right = di['k']
+	di['n'].depth = 2
 
 	di['m'].left = di['g']
 	di['m'].right = di['o']
+	di['m'].depth = 2
 
 	di['s'].left = di['h']
-	di['s'].right = di['f']
+	di['s'].right = di['v']
+	di['s'].depth = 3
 
 	di['u'].left = di['f']
-	di['u'].right = None
+	di['u'].right = copy.deepcopy(Empty)
+	di['u'].depth = 3
 
 	di['r'].left = di['l']
-	di['r'].right = None
+	di['r'].right = copy.deepcopy(Empty)
+	di['r'].depth = 3
 
 	di['w'].left = di['p']
 	di['w'].right = di['j']
+	di['w'].depth = 3
 
 	di['d'].left = di['b']
 	di['d'].right = di['x']
+	di['d'].depth = 3
 
 	di['k'].left = di['c']
 	di['k'].right = di['y']
+	di['k'].depth = 3
 
 	di['g'].left = di['z']
 	di['g'].right = di['q']
+	di['g'].depth = 3
 
-	di['o'].left = None
-	di['o'].right = None
+	di['o'].left = copy.deepcopy(Empty)
+	di['o'].right = copy.deepcopy(Empty)
+	di['o'].depth = 3
 
+	# di['h'].left = di['5']
+	# di['h'].right = di['4']
+	# di['h'].depth = 4
+
+	# di['v'].left = copy.deepcopy(Empty)
+	# di['v'].right = di['3']
+	# di['v'].depth = 4
+
+	# di['f'].left = copy.deepcopy(Empty)
+	# di['f'].right = copy.deepcopy(Empty)
+	# di['f'].depth = 4
+
+	# di['o'].left = copy.deepcopy(Empty)
+	# di['o'].right = copy.deepcopy(Empty)
+	# di['o'].depth = 3
+
+	# di['o'].left = copy.deepcopy(Empty)
+	# di['o'].right = copy.deepcopy(Empty)
+	# di['o'].depth = 3
+
+	# di['o'].left = copy.deepcopy(Empty)
+	# di['o'].right = copy.deepcopy(Empty)
+	# di['o'].depth = 3
+
+	# di['o'].left = copy.deepcopy(Empty)
+	# di['o'].right = copy.deepcopy(Empty)
+	# di['o'].depth = 3
+
+	# di['o'].left = copy.deepcopy(Empty)
+	# di['o'].right = copy.deepcopy(Empty)
+	# di['o'].depth = 3
+
+	# di['o'].left = copy.deepcopy(Empty)
+	# di['o'].right = copy.deepcopy(Empty)
+	# di['o'].depth = 3
+
+	# di['o'].left = copy.deepcopy(Empty)
+	# di['o'].right = copy.deepcopy(Empty)
+	# di['o'].depth = 3
+
+	# di['o'].left = copy.deepcopy(Empty)
+	# di['o'].right = copy.deepcopy(Empty)
+	# di['o'].depth = 3
+
+	# di['o'].left = copy.deepcopy(Empty)
+	# di['o'].right = copy.deepcopy(Empty)
+	# di['o'].depth = 3
+
+	# di['o'].left = copy.deepcopy(Empty)
+	# di['o'].right = copy.deepcopy(Empty)
+	# di['o'].depth = 3
+
+	# di['o'].left = copy.deepcopy(Empty)
+	# di['o'].right = copy.deepcopy(Empty)
+	# di['o'].depth = 3
+
+	# di['o'].left = copy.deepcopy(Empty)
+	# di['o'].right = copy.deepcopy(Empty)
+	# di['o'].depth = 3
+
+	# di['o'].left = copy.deepcopy(Empty)
+	# di['o'].right = copy.deepcopy(Empty)
+	# di['o'].depth = 3
 	# print(mainTree.root.left.left.value)
 	return mainTree
 
